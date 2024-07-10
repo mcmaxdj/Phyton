a7325ada185618b9158921b7732b56041ea16b01
Maksym Sobetskyi <mcmaxdj@gmail.com>
2024-07-10 17:41:17 +0200

    adding viriables and some print

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print('Hello, world')
+i = 1
+test = 'This is a test'
+foo = "bar"
 
-# Duck typing method
+my_age = 40
+my_age += 1
+my_name = "Maksym"
+
+my_data = my_name + str(my_age)
+
+print(my_data)
+
