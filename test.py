8ae58b01b74e2cf236cab5c8c04e621ccab9039c
Maksym Sobetskyi <mcmaxdj@gmail.com>
2024-07-11 11:32:20 +0200

    new if, elif, else example

--- test.py
+++ test.py
@@ -1,12 +1,40 @@
-i = 1
-test = 'This is a test'
-foo = "bar"
+# i = 1
+# test = 'This is a test'
+# foo = "bar"
 
-my_age = 40
-my_age += 1
-my_name = "Maksym"
+# my_age = 40
+# my_age += 1
+# my_name = "Maksym"
 
-my_data = my_name + str(my_age)
+# my_data = my_name + str(my_age)
 
-print(my_data)
+# print(my_data)
 
+"""
+    how to write code
+"""
+
+# class MyTest:
+#     x = 1
+
+# def my_func():
+#     return 1
+
+# x = 12
+# if x >= 0:
+#     print('positive number')
+# elif x <= 0:
+#     print('negative number')
+# else:
+#     print('what is it?')
+
+y = 11
+if y == 0:
+    print('zero')
+elif y > 0:
+    if y % 2:
+        print('this number is odd')
+    else:
+        print('this number is even')
+else:
+    print('Negative')
